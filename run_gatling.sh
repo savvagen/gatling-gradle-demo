8b6cd46929499c5653c7c646d24647c436532d0e
savva.genchevskiy <savva.genchevskiy@ab-soft.net>
2021-09-07 16:02:03 +0300

    code refactoring

--- run_gatling.sh
+++ run_gatling.sh
@@ -3,5 +3,5 @@
 
 GATLING_VERSION=${GATLING_VERSION:-3.6.1}
 
-./gatling-"${GATLING_VERSION}"/bin/gatling.sh -sf src/gatling/scala/absoft/com/beats/simulations -s absoft.com.beats.simulations.LoadSimulation -rf $(pwd)
+./gatling-"${GATLING_VERSION}"/bin/gatling.sh -sf src/gatling/scala/org/example/simulations -s org.example.simulations.LoadSimulation -rf $(pwd)
 
--- run_gatling_no_reports.sh
+++ run_gatling_no_reports.sh
@@ -3,4 +3,4 @@
 
 GATLING_VERSION=${GATLING_VERSION:-3.6.1}
 
-./gatling-"${GATLING_VERSION}"/bin/gatling.sh -sf src/gatling/scala/absoft/com/beats/simulations -nr -s absoft.com.beats.simulations.LoadSimulation > /gatling/run.log
+./gatling-"${GATLING_VERSION}"/bin/gatling.sh -sf src/gatling/scala/org/example/simulations -nr -s org.example.simulations.LoadSimulation > /gatling/run.log
